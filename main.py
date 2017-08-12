f0453924cba68ce8d37f4db7f4e2f84b94e670ce
Florian Jourda <florian.jourda@gmail.com>
2017-08-12 13:10:45 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import os
 from airtable import airtable
 from eventbrite import Eventbrite
@@ -7,8 +8,8 @@ AIRTABLE_BASE_ID = os.getenv('EVENT_AIRTABLE_BASE_ID')
 AIRTABLE_API_KEY = os.getenv('EVENT_AIRTABLE_API_KEY')
 AIRTABLE_EVENTS_TABLE_ID = 'tblrq7mgDr3bH5P24'
 AIRTABLE_EVENTS_VIEW_ID = 'viwnHWHuNNn9PEfrR'
-AIRTABLE_CONTACT_TABLE_ID = 'tbloEdq8Zw8GQ6l9R'
-AIRTABLE_ORDERS_TABLE_ID = 'tblwjy7s3eeh8DWtT'
+AIRTABLE_CONTACTS_TABLE_ID = 'tbloEdq8Zw8GQ6l9R'
+AIRTABLE_TICKETS_TABLE_ID = 'tblwjy7s3eeh8DWtT'
 
 print('Get events from Airtable')
 airtable_client = airtable.Airtable(AIRTABLE_BASE_ID, AIRTABLE_API_KEY)
@@ -23,52 +24,76 @@ airtable_last_event = next(reversed(airtable_events))
 last_event_eventbrite_id = airtable_last_event.get('fields').get('Eventbrite Id')
 
 print('Get emails from our community contacts in Airtable')
-airtable_contacts = airtable_client.get(AIRTABLE_CONTACT_TABLE_ID).get('records')
+airtable_contacts = airtable_client.iterate(AIRTABLE_CONTACTS_TABLE_ID)
+def a(b):
+  print(b.get('fields').get('Email'))
+  return ''
+
 airtable_contact_by_email = {
+  # 'a': airtable_contact
   airtable_contact.get('fields').get('Email'): airtable_contact
   for airtable_contact in airtable_contacts
 }
 
 print('Get tickets already copied to Airtable')
-airtable_tickets = airtable_client.get(AIRTABLE_ORDERS_TABLE_ID).get('records')
-airtable_ticket_by_order_id = {
-  airtable_ticket.get('fields').get('Order Id'): airtable_ticket
+airtable_tickets = airtable_client.get(AIRTABLE_TICKETS_TABLE_ID).get('records')
+airtable_ticket_by_ticket_id = {
+  airtable_ticket.get('fields').get('Ticket Id'): airtable_ticket
   for airtable_ticket in airtable_tickets
 }
 
 print('Get attendees from Eventbrite')
 eventbrite = Eventbrite(EVENTBRITE_API_SECRET)
-eventbrite_last_event_orders = eventbrite.get(
+eventbrite_last_event_attendees= eventbrite.get(
   '/events/{}/attendees/'.format(last_event_eventbrite_id)
 )
 
-for eventbrite_order in eventbrite_last_event_orders.get('attendees'):
-  eventbrite_profile = eventbrite_order.get('profile')
-  order_id = int(eventbrite_order.get('order_id'))
+def _create_or_update_on_airtable(airtable_id, airtable_current_record, new_params):
+  if airtable_current_record:
+    airtable_current_params = {
+      key: airtable_current_record.get('fields').get(key) for key, value in new_params.items()
+    }
+    if new_params != airtable_current_params:
+      print('Need to update')
+      airtable_record = airtable_client.update(airtable_id, str(airtable_current_record.get('id')), new_params)
+    else:
+      # print('{} is up to date'.format(airtable_current_record.get('id')))
+      airtable_record = airtable_current_record
+  else:
+    print('Need to create')
+    airtable_record = airtable_client.create(airtable_id, new_params)
+  return airtable_record
+
+n = 0
+for eventbrite_attendee in eventbrite_last_event_attendees.get('attendees'):
+  n = n + 1
+  ticket_id = int(eventbrite_attendee.get('id'))
+
+  eventbrite_contact = eventbrite_attendee.get('profile')
+  email = eventbrite_contact.get('email')
+  print("{}: {}".format(n, email))
+
+  eventbrite_contact_params = {
+    'Name': eventbrite_contact.get('name').title(),
+    'Email': email,
+    'Age': eventbrite_contact.get('age')
+  }
+  current_airtable_contact = airtable_contact_by_email.get(email)
+  airtable_contact = _create_or_update_on_airtable(AIRTABLE_CONTACTS_TABLE_ID, current_airtable_contact, eventbrite_contact_params)
+  # Need to update the cache of contacts as multiple orders can link to the same contact
+  # and will need a fresh version of it.
+  airtable_contact_by_email[airtable_contact.get('fields').get('Email')] = airtable_contact
 
   eventbrite_ticket_params = {
-    'Order Id': order_id,
-    'Name': eventbrite_profile.get('name'),
-    'Email': eventbrite_profile.get('email'),
-    'Status': eventbrite_order.get('status'),
-    'Type': eventbrite_order.get('ticket_class_name'),
-    'Event': [
-        airtable_event_key_by_evenbrite_id[int(eventbrite_order.get('event_id'))].decode('utf-8'),
+    'Ticket Id': ticket_id,
+    'Order Id': int(eventbrite_attendee.get('order_id')),
+    '🗃 Contact': [airtable_contact.get('id')],
+    'Status': eventbrite_attendee.get('status'),
+    'Type': eventbrite_attendee.get('ticket_class_name'),
+    '🗓 Event': [
+        airtable_event_key_by_evenbrite_id[int(eventbrite_attendee.get('event_id'))],
      ],
-     'Checked In?': True if eventbrite_order.get('checked_in') else None,
+     'Checked In?': True if eventbrite_attendee.get('checked_in') else None,
   }
-
-  airtable_ticket = airtable_ticket_by_order_id.get(order_id)
-
-  if airtable_ticket:
-    airtable_ticket_params = {
-      key: airtable_ticket.get('fields').get(key) for key, value in eventbrite_ticket_params.items()
-    }
-    if eventbrite_ticket_params != airtable_ticket_params:
-      res = airtable_client.update(AIRTABLE_ORDERS_TABLE_ID, str(airtable_ticket.get('id')), eventbrite_ticket_params)
-      print(res)
-    else:
-      print('{} is up to date'.format(airtable_ticket_params.get('Order Id')))
-  else:
-    res = airtable_client.create(AIRTABLE_ORDERS_TABLE_ID, eventbrite_ticket_params)
-    print(res)
+  current_airtable_ticket = airtable_ticket_by_ticket_id.get(ticket_id)
+  airtable_ticket = _create_or_update_on_airtable(AIRTABLE_TICKETS_TABLE_ID, current_airtable_ticket, eventbrite_ticket_params)
