e004c26b30e06cc7f89f3927b4c46abc0484d9a5
Florian Jourda <florian.jourda@gmail.com>
2017-08-14 17:26:57 +0200

    Fix path for modules

--- manual-bulk/manual-bulk.py
+++ manual-bulk/manual-bulk.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../eventbrite_webhook'))
 from airtable import airtable
 from eventbrite import Eventbrite
 
